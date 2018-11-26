659276d7ceb7d25b18406db82740100d8bf2b267
Kevin Kessler <KevinKesslerIT@gmail.com>
2018-11-26 15:54:15 -0500

    Added ipv4 forwarding

--- install.sh
+++ install.sh
@@ -114,8 +114,8 @@ wpa_key_mgmt=WPA-PSK
 wpa_pairwise=TKIP
 rsn_pairwise=CCMP
 " > /etc/hostapd/hostapd.conf
-
-
+#ipv4 forwarding!
+sysctl -w net.ipv4.ip_forward=1
 #setup iptables rules
 iptables -t nat -A POSTROUTING -o wlan0 -j MASQUERADE  
 iptables -A FORWARD -i wlan0 -o $interface -m state --state RELATED,ESTABLISHED -j ACCEPT  
